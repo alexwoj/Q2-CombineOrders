95c3745391ae910ab15118b4d3f6ddb2250d58ee
alex.wojtowicz@gmail.com <git@warpnet.com.br>
2022-10-19 16:43:00 -0300

    first version with working example, passes all tests

---
+++ combineorders/tests/test_orders.py
@@ -0,0 +1,15 @@
+import unittest
+from combineorders.orders import Orders
+
+class TestCombineOrders(unittest.TestCase):
+
+    def setUp(self):
+        self.orders = [70, 30, 10]
+        self.n_max = 100
+        self.expected_orders = 2
+
+    def test_combine_orders(self):
+
+        how_many = Orders().combine_orders(self.orders, self.n_max)
+        self.assertEqual(self.expected_orders, how_many)
+        self.assertIsInstance(how_many, int)
